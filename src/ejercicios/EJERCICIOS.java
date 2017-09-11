16bf6bbf789bc3e205bffb0f880efccdf0683af7
alumno <alumno@LIMSFLAB06-06D.system-uap.adm>
2017-09-11 18:55:22 -0500

    001commit

---
+++ src/ejercicios/EJERCICIOS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class EJERCICIOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+        
+    }
+    
+}
